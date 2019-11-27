fbd8950f50d406e7a1af5978d63e6ea0c417b5f5
xoxfabxox <57392074+xoxfabxox@users.noreply.github.com>
2019-11-27 23:45:10 +0100

    Update pythonpackage.py

--- .github/workflows/pythonpackage.py
+++ .github/workflows/pythonpackage.py
@@ -1,22 +1,22 @@
 import itertools
 
 
-def win(jeux_en_cour):
+def win(jeux_en_cour):        #Condition de victoire
 
     def all_same(l):
-        if l.count(l[0]) == len(l) and l[0] != 0:
+        if l.count(l[0]) == len(l) and l[0] != 0:        #On regarde si tout les element de la -liste- sont les memes ( verticale / horizontale )
             return True
         else:
             return False
 
-    # horizontal
+    # horizontal victoire
     for ligne in game:
         print(ligne)
         if all_same(ligne):
             print(f"Player {ligne[0]} Gagne a l'horizontale!")
             return True
 
-    # vertical
+    # vertical victoire
     for collone in range(len(game[0])):
         check = []
         for ligne in game:
@@ -46,18 +46,18 @@ def win(jeux_en_cour):
     return False
 
 
-def plateau_de_jeux(game_map, player=0, ligne=0, col=0, just_display=False):
+def plateau_de_jeux(game_map, player=0, ligne=0, col=0, just_display=False):     #Ajout de paramettre dans plateau de jeux
 
-    try:
+    try:           #Pour éviter les erreurs, on prévois une solution
         if game_map[ligne][col] != 0:
             print("Espace occuper ! Essaye ailleur")
             return False
 
         print("   "+"  ".join([str(i) for i in range(len(game_map))]))
         if not just_display:
-            game_map[ligne][col] = player
+            game_map[ligne][col] = player      #Choix de la case en fonction du joueur
         for count, ligne in enumerate(game_map):
-            print(count, ligne)
+            print(count, ligne)   #Affichage Ligne col
         return game_map
     except IndexError:
         print("Erreur: Assure toi que Ligne et Colonne sont à 0, 1 ou 2. --> (IndexError)")
@@ -69,7 +69,7 @@ def plateau_de_jeux(game_map, player=0, ligne=0, col=0, just_display=False):
 
 play = True
 players = [1, 2]
-while play:
+while play:                     #Aire de jeux dans des tableaux
     game = [[0, 0, 0],
             [0, 0, 0],
             [0, 0, 0]]
@@ -82,11 +82,11 @@ while play:
         played = False
         while not played:
             print(f"Player: {current_player}")
-            col_choice = int(input("Colonne ? "))
+            col_choice = int(input("Colonne ? "))    #Choix de l'endroit ou l'on joue
             ligne_choice = int(input("Ligne? "))
             played = plateau_de_jeux(game, player=current_player, ligne=ligne_choice, col=col_choice)
 
-        if win(game):
+        if win(game):    #Fin de la partie
             game_won = True
             again = input("Patie terminer ! Voulez-vous rejouer ? (y/n) ")
             if again.lower() == "y":
